aaaa153f85d74d946ba71f5cee704257553da982
= <akashsarada@gmail.com>
2025-02-01 20:51:54 -0500

    added functionality to read from database

--- backend/database.py
+++ backend/database.py
@@ -44,6 +44,50 @@ def write_startup(startup_in_json):
         if 'client' in locals() and client:  # Ensure client is defined and not None
             client.close()
 
+def read_startup(input_json):
+    try:
+        # 1. Establish a connection to MongoDB
+        load_dotenv("mongo.env")
+        username= os.getenv('MONGO_USERNAME')
+        password= os.getenv('MONGO_PASSWORD')
+
+        client = MongoClient(f"mongodb+srv://{username}:{password}@pitchmatch.ehi9k.mongodb.net/?retryWrites=true&w=majority&appName=pitchmatch")
+        db = client["pitchmatch"]
+        collection = db["startup_info"]
+
+        # 2. Read data from MongoDB
+        cursor = collection.find(input_json)
+        for document in cursor:
+            print(document)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    finally:
+        if 'client' in locals() and client:  # Ensure client is defined and not None
+            client.close()
+
+
+def read_vc(vc_json):
+    try:
+        # 1. Establish a connection to MongoDB
+        load_dotenv("mongo.env")
+        username = os.getenv('MONGO_USERNAME')
+        password = os.getenv('MONGO_PASSWORD')
+
+        client = MongoClient(f"mongodb+srv://{username}:{password}@pitchmatch.ehi9k.mongodb.net/?retryWrites=true&w=majority&appName=pitchmatch")
+        db = client["pitchmatch"]
+        collection = db["startup_info"]
+
+        # 2. Read data from MongoDB
+        cursor = collection.find(vc_json)
+        for document in cursor:
+            print(document)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    finally:
+        if 'client' in locals() and client:  # Ensure client is defined and not None
+            client.close()
 
 # --- Example Usage ---
 if __name__ == "__main__":
@@ -56,10 +100,8 @@ if __name__ == "__main__":
         "startup_name": "test_startup",
         "startup_email": "test@startup.com"})
 
+    read_vc({
+        "vc_name": "test_vc"})
 
-    # Example 2: Using a connection string with SRV record (for MongoDB Atlas)
-    # connection_string = "mongodb+srv://your_username:your_password@your_cluster_name.mongodb.net/?retryWrites=true&w=majority"
-    # database_name = "your_database_name"
-    # collection_name = "your_collection_name"
-
-    # connect_and_read_mongodb(connection_string, database_name, collection_name)
+    read_startup({
+        "startup_name": "test_startup"})
